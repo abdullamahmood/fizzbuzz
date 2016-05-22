0b6cffa3331d617fd8e3a8dfc4ee51c19eb45148
Abdulla <abdulla.ali.mahmood@gmail.com>
2016-05-22 23:33:59 +0100

    completed fizzbuzz test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,10 @@
 def fizzbuzz(number)
-	if number == 3 then
+	if number % 5 == 0 && number % 3 == 0 then
+		return 'fizzbuzz'
+	elsif number % 3 == 0  then
 		return 'fizz'
+	elsif number % 5 == 0 then
+		return 'buzz'
 	else
 		return number
 	end
